43f5b58c64c59a91f18a36a75c507cff24e0f2bd
LAPTOP-I3DQV4VE\Chaudhary <aounshabbir@gmail.com>
2023-02-07 22:53:22 +0500

    Add project files.

---
+++ Controllers/CartController.cs
@@ -0,0 +1,21 @@
+﻿using Cart_Service.Services;
+using Microsoft.AspNetCore.Mvc;
+using Users_Service;
+
+namespace Cart_Service.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CartController : ControllerBase
+    {
+
+        // GET api/<CartController>/5
+        [HttpGet("{id}")]
+        public Task<UserResponse> GetUserById(int id)
+        {
+            var cartService = new CartService();
+            var user = cartService.GetUserById(id);
+            return user;
+        }
+    }
+}
---
+++ Services/CartService.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Core;
+using Grpc.Net.Client;
+using Users_Service;
+
+
+namespace Cart_Service.Services
+{
+      public class CartService
+        {
+            public async Task<UserResponse> GetUserById(int userId)
+            {
+                var channel = GrpcChannel.ForAddress("https://localhost:7249");
+                var client = new User.UserClient(channel);
+                var request = new UserRequest { Id = userId };
+                var response = await client.GetUserByIdAsync(request);
+                channel.ShutdownAsync().Wait();
+                return response;
+            }
+        }
+    }
